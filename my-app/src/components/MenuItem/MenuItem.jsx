85ed1189d7b5a858f330feb222e25d24fdbf8c25
Harsha I <P00999850@asianpaints.com>
2025-08-01 15:14:44 +0530

    added the image , some logic

--- my-app/src/components/MenuItem/MenuItem.jsx
+++ my-app/src/components/MenuItem/MenuItem.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const MenuItem = ({ item, onAdd }) => (
   <div className="menu-item">
+    <img src={item.image} alt={item.name} className="menu-image" />
     <h3>{item.name}</h3>
     <p>₹{item.price}</p>
-    <button onClick={() => onAdd(item)}>Add to Cart</button>
+    <button className="cart-button" onClick={() => onAdd(item)}>Add to Cart</button>
   </div>
 );
 
--- my-app/src/components/container/OrderContainer.jsx
+++ my-app/src/components/container/OrderContainer.jsx
@@ -10,19 +10,24 @@ const OrderContainer = () => {
   };
 
   const menuItems = [
-    { id: 1, name: 'Espresso', price: 120 },
-    { id: 2, name: 'Cappuccino', price: 150 },
-    { id: 3, name: 'Blueberry Muffin', price: 90 },
-    { id: 4, name: 'Croissant', price: 100 },
+    { id: 1, name: 'Espresso', price: 120 , image:"/MenuItem/Espresso.jpeg"},
+    { id: 2, name: 'Cappuccino', price: 150 ,image:""},
+    { id: 3, name: 'Blueberry Muffin', price: 90 ,image:"/MenuItem/Muffin.jpg"},
+    { id: 4, name: 'Croissant', price: 100 , image: '/MenuItem/Croissant.jpg' },
   ];
 
   return (
     <div className="order-container">
-      <h2>☕ Welcome to JavaJoy Café</h2>
+      <h2>☕ Welcome to Meteor Café</h2>
+      <div className='container text-center p-3'>
+      <div className='row text-center justify-content-center align-items-center'>
       <div className="menu-section">
+        
         {menuItems.map((item) => (
           <MenuItem key={item.id} item={item} onAdd={handleAddToCart} />
         ))}
+      </div>
+      </div>
       </div>
       <CartSummary cart={cart} />
       <button onClick={() => alert('Order placed!')} className="order-button">
